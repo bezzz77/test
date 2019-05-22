b62cb1f0ef1c5ea321e81727aeb13e5f4dee7596
bezzz77 <bezzz77@gmail.com>
2019-05-22 13:15:51 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items:1,
+    autoplay: 0,
+    navigation: true,
+    autoplaySpeed: 1000,
+    itemsDesktop: [1199, 1],
+    itemsMobile: [479, 1],
+    dots : true,
+    navClass : true,
+  }
+
+  );
+
+  var owl = $('.owl-carousel');
+  owl.owlCarousel();
+  
+  $('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+  })
+  // Go to the previous item
+  $('.customPrevBtn').click(function() {
+      // With optional speed parameter
+      // Parameters has to be in square bracket '[]'
+      owl.trigger('prev.owl.carousel', [300]);
+  })
+
+});
